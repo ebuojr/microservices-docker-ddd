770f9e0c51e603a1e3d8d174dbeb2cc836ffcb3f
Ibrahim <ebuo12@gmail.com>
2020-10-28 16:50:36 +0100

    catalogApi is ready with docker and mongodb hosted on docker
    fremskridt

---
+++ microserviceddddocker/src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Catalog.API.Setting;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(ICatalogDatabaseSetting setting)
+        {
+            var client = new MongoClient(setting.ConnectionString);
+            var database = client.GetDatabase(setting.DatabaseName);
+
+            Products = database.GetCollection<Product>(setting.CollectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ microserviceddddocker/src/Catalog/Catalog.API/Setting/ICatalogDatabaseSetting.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Catalog.API.Setting
+{
+    public interface ICatalogDatabaseSetting
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
